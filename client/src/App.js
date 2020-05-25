71a14fa859d53f547656df3f3500b5ee58d2fd7d
Siddhi Goyal <siddhigoyalsi@gmail.com>
2020-05-25 15:54:03 +0530

    Completed Authentication Part

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,47 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route} from  'react-router-dom'
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser, logoutUser } from './actions/authActions'
+import { Provider} from 'react-redux';
+import store from './store'
 
 import NavBar from './Components/layout/NavBar';
 import Footer from './Components/layout/Footer';
 import Landing from './Components/layout/Landing';
-import Login from './Components/auth/login';
-import Register from './Components/auth/register';
-
+import Login from './Components/auth/Login';
+import Register from './Components/auth/Register';
 
 
 import './App.css';
 
+if(localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    //TODO: Clear current Profile
+    //Redirect to login
+
+    window.location.href = '/login';
+  }
+}
+
 class App extends Component {
   render () {
     return (
+      <Provider store={store}>
       <Router>
       <div className="App">
         <NavBar />
@@ -25,6 +53,7 @@ class App extends Component {
         <Footer />
       </div>
       </Router>
+      </Provider>
     );
   }
 }
--- client/src/Components/layout/NavBar.js
+++ client/src/Components/layout/NavBar.js
@@ -1,9 +1,55 @@
 import React, { Component} from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+import { logoutUser} from '../../actions/authActions'
 
 class NavBar extends Component {
 
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
+
   render() {
+
+    const {isAuthenticated, user} = this.props.auth;
+    const guestLinks = (
+
+      <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link className="nav-link" to="/register">
+          Sign Up
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/login">
+          Login
+        </Link>
+      </li>
+      </ul>
+    );
+
+    const authLinks = (
+
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a href="/" 
+            onClick={this.onLogoutClick.bind(this)}className="nav-link"
+          >
+            <img
+              className="rounded-circle" 
+              src={user.avatar} 
+              alt={user.name} 
+              title="You must have a Gravatar connected to your gmail"
+              style={{width: '25px', marginRight: '5px'}}
+              />{' '}
+              Logout
+          </a>
+        </li>
+      </ul>
+    );
+
     return (
       <div>
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
@@ -23,19 +69,7 @@ class NavBar extends Component {
             </Link>
           </li>
         </ul>
-
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">
-              Sign Up
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
+        {isAuthenticated ? authLinks : guestLinks}
       </div>
     </div>
   </nav>
@@ -44,4 +78,13 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+NavBar.protoTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, {logoutUser})(NavBar);
